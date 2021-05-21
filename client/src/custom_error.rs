02309492c922a472bddd998afec5d7d9f0b06541
matt <syan4@ualberta.ca>
2021-05-21 13:05:17 -0600

    refactor using thiserror

--- client/src/custom_error.rs
+++ client/src/custom_error.rs
@@ -6,63 +6,28 @@ use shared::deps::tokio as tokio;
 use crossterm::ErrorKind;
 use tokio::task;
 use std::fmt::Formatter;
+use shared::deps::thiserror as thiserror;
+use thiserror::Error;
+use shared::deps::tokio::sync::broadcast::error::RecvError;
+
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 
-
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
-    Connection(std::io::Error),
-    Control(crossterm::ErrorKind),
-    EventListeningTaskIncomplete(shared::error_fill::JoinError),
-    MessageSendingTaskIncomplete(shared::error_fill::JoinError),
-}
-
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-
-        match *self {
-            Error::Connection(..) =>
-                write!(f, "Failed to establish connection to server"),
-            // The wrapped error contains additional information and is available
-            // via the source() method.
-            Error::Control(..) =>
-                write!(f, "Failed to read control event"),
-            Error::EventListeningTaskIncomplete(..) =>
-                write!(f, "Control event listening task failed to complete"),
-            Error::MessageSendingTaskIncomplete(..) =>
-                write!(f, "Message sending task failed to complete"),
-
-        }
-    }
-}
-
-
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Error {
-        Error::Connection(err)
-    }
-}
-
-
-impl From<crossterm::ErrorKind> for Error {
-    fn from(err: crossterm::ErrorKind) -> Error {
-        Error::Control(err)
-    }
-}
-
-
-
-
-impl error::Error for Error {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match *self {
-            Error::Connection(ref e) => Some(e),
-            Error::Control(ref e) => Some(e),
-            Error::EventListeningTaskIncomplete(ref e) => Some(e),
-            Error::MessageSendingTaskIncomplete(ref e) => Some(e),
-        }
-    }
+    #[error("Failed to establish connection to server")]
+    Connection(#[source] std::io::Error),
+    #[error("Terminal initialization failed")]
+    TerminalInitialization(#[source] std::io::Error),
+    #[error("Failed to draw frame")]
+    DrawFrame(#[source] std::io::Error),
+    #[error("Failed to read control event")]
+    Control(#[from] crossterm::ErrorKind),
+    #[error("Control event listening task failed to complete")]
+    EventListeningTaskIncomplete(#[source] shared::error_fill::JoinError),
+    #[error("Message sending task failed to complete")]
+    MessageSendingTaskIncomplete(#[source] shared::error_fill::JoinError),
+    #[error("Drawing sending task failed to complete")]
+    DrawTaskIncomplete(#[source] shared::error_fill::JoinError),
 }
