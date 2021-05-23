0f788f16c628dbf4f81c39e73f5baf9bd001454f
matt <syan4@ualberta.ca>
2021-05-23 12:42:40 -0600

    extract run from main, recursively print error sources, reply client ping

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,42 +1,71 @@
-use std::str::FromStr;
+use shared::dep::bytes as bytes;
+use bytes::Bytes;
 
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
-use tokio_stream::{Stream, StreamExt};
-use tokio_util::codec::{AnyDelimiterCodec, FramedRead, LinesCodec};
-
-use shared::deps::tokio as tokio;
-use shared::message::{CLIENT_QUIT_MESSAGE, ClientToServerMessage, MESSAGE_DELIMITER};
-
-use crate::custom_errors::Error;
-
-mod custom_errors;
+use tokio_stream::{StreamExt};
 
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+use shared::dep::tokio as tokio;
+
+use shared::dep::serde_cbor;
+use shared::message::{CLIENT_QUIT_MESSAGE, ClientToServerMessage, ServerToClientMessage};
+
+use crate::error::Error;
+use shared::dep::futures::{SinkExt, TryFutureExt};
+use error::Result;
+
+
+mod error;
+
+
+async fn run() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.map_err(|e| Error::Bind(e))?;
 
     loop {
         println!("Waiting for new connection...");
-        let (mut socket, address) = listener.accept().await?;
+        let (socket, address) = listener.accept().await.map_err(|e| Error::Accept(e))?;
         println!("Accepted connection from {}", address);
         tokio::spawn(async move {
-
             let mut framed = shared::message::default_framed(socket);
 
 
-            while let Some(received_bytes) = framed.next().await{
-                let received_bytes = received_bytes?;
-                let received_string = String::from_utf8(Vec::from(&received_bytes[..]))?;
-                println!("{}", received_string);
-                if received_string == CLIENT_QUIT_MESSAGE.to_string() {break}
+            while let Some(received_bytes) = framed.next().await {
+                let received_bytes = received_bytes.map_err(|e| Error::AbruptClientLeave(e))?;
+                let received = serde_cbor::from_slice::<ClientToServerMessage>(&*received_bytes).map_err(|e| Error::DeserializationError(e))?;
+
+                println!("{:?}", received);
+                match received {
+                    CLIENT_QUIT_MESSAGE => {
+                        break;
+                    }
+                    ClientToServerMessage::W => {}
+                    ClientToServerMessage::A => {}
+                    ClientToServerMessage::S => {}
+                    ClientToServerMessage::D => {}
+                    ClientToServerMessage::J => {}
+                    ClientToServerMessage::Spacebar => {}
+                    ClientToServerMessage::P => {}
+                    ClientToServerMessage::Ping(time) => {
+                        println!("Sending Pong back to {}", address);
+                        framed.send(Bytes::from(serde_cbor::to_vec(&ServerToClientMessage::Pong(time)).unwrap())).await
+                            .map_err(|e| Error::SendPong(e))?
+                    }
+                }
             }
-
             println!("Client {} leaves", address);
+            error::ok()
+        }.map_err(|x| {
+            shared::error_util::eprint_error_chain(x);
+        })
+        );
+    }
+}
 
 
-        Ok::<(), Error>(())
-        });
+#[tokio::main]
+async fn main() -> () {
+    if let Err(e) = run().await {
+        shared::error_util::eprint_error_chain(e);
+        std::process::exit(-1);
     }
 }
