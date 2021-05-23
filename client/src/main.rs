1adf3b297052319965b2264a2fbde942e6aaf1d8
matt <syan4@ualberta.ca>
2021-05-23 12:39:39 -0600

    remove unused uses

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,15 +1,4 @@
-use std::{fmt, io};
-use std::error::Error;
-
-use tokio::io::AsyncWriteExt;
-use tokio::net::TcpStream;
-use tokio::runtime::Handle;
-use tokio::time::{Duration, sleep};
-
-use shared::deps::tokio as tokio;
-
-
-
+use shared::dep::tokio as tokio;
 
 #[tokio::main]
 async fn main() {
