3008d951eadada320c6ed8f51b3cf63a6054d0a8
matt <syan4@ualberta.ca>
2021-05-27 12:28:31 -0600

    always better error handling utils

--- shared/src/error_util.rs
+++ shared/src/error_util.rs
@@ -1,7 +1,5 @@
-
-
-pub fn eprint_error_chain(e: impl std::error::Error){
-    if let Some(source) = e.source(){
+pub fn eprint_error_chain(e: impl std::error::Error) {
+    if let Some(source) = e.source() {
         eprint_error_chain(source);
         println!("↓");
     }
