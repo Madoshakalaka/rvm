b2349eac76a7188d4c2db4081c2dab55f9a271b8
matt <syan4@ualberta.ca>
2021-05-27 12:33:07 -0600

    logging start and end handling

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,25 @@
-use shared::dep::tokio as tokio;
+use shared::dep::tokio;
+use client::log;
+
+
+use std::time::Duration;
+use tracing::info;
+
 
 #[tokio::main]
 async fn main() {
-    if let Err(e) = client::run().await {
-        shared::error_util::eprint_error_chain(e);
-        std::process::exit(-1);
-    };
+    let (main_end_tx, main_end_rx) = tokio::sync::oneshot::channel::<()>();
+    let logging = tokio::spawn(log::start_logging(main_end_rx));
+
+
+    let run_res = client::run().await;
+
+    info!("Main exit");
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    main_end_tx.send(()).unwrap();
+    logging.await.unwrap();
+
+    if run_res.is_err() {
+        std::process::exit(-1)
+    }
 }
