edcd38b5f27c399a8dab226f8794004bd9cce00e
matt <syan4@ualberta.ca>
2021-05-30 07:06:02 -0600

    you can't hate raw_mode and alternate terminal

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,11 +3,15 @@ use client::log;
 
 
 use std::time::Duration;
-use shared::dep::tracing::info;
+use shared::dep::{tracing::info, crossterm::{self, execute, terminal::{EnterAlternateScreen, LeaveAlternateScreen}}};
 
 
 #[tokio::main]
 async fn main() {
+
+    crossterm::terminal::enable_raw_mode().unwrap();
+    execute!(std::io::stdout(), EnterAlternateScreen).unwrap();
+
     let (main_end_tx, main_end_rx) = tokio::sync::oneshot::channel::<()>();
     let logging = tokio::spawn(log::start_logging(main_end_rx));
 
@@ -18,6 +22,10 @@ async fn main() {
     tokio::time::sleep(Duration::from_millis(100)).await;
     main_end_tx.send(()).unwrap();
     logging.await.unwrap().unwrap();
+
+
+    crossterm::terminal::disable_raw_mode().unwrap();
+    execute!(std::io::stdout(), LeaveAlternateScreen).unwrap();
 
     if run_res.is_err() {
         std::process::exit(-1)
